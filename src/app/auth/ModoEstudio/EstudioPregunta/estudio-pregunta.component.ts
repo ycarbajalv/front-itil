f37f16cd547cc806333fc0e99f38661dea1f7935
Max Mantilla Rodríguez <mmantilla@bsginstitute.com>
2022-08-27 10:58:24 -0500

    actualización

--- src/app/auth/ModoEstudio/EstudioPregunta/estudio-pregunta.component.ts
+++ src/app/auth/ModoEstudio/EstudioPregunta/estudio-pregunta.component.ts
@@ -105,22 +105,32 @@ export class EstudioPreguntaComponent implements OnInit {
     })
   }
   chageRadio(value: number,i: number, j: number) {
-    this.RespuestaMarcada=false;
-      if (value == 0 && this.ListaPreguntas[i].pregunta.respuesta[j] && this.ListaPreguntas[i].pregunta.idSimuladorTipoRespuesta==1) {
-        this.ListaPreguntas[i].pregunta.respuesta.forEach((x:any)=>{
-          x.respuestaSelecionada=0
-        })
-        this.RespuestaMarcada=true;
-        return 1;
-      }
-      if (value == 0 && this.ListaPreguntas[i].pregunta.respuesta[j] && this.ListaPreguntas[i].pregunta.idSimuladorTipoRespuesta==5) {
-        this.RespuestaMarcada=true
-        return 1;
-      }
-      this.RespuestaMarcada=false;
+    if (value == 0 && this.ListaPreguntas[i].pregunta.respuesta[j] && this.ListaPreguntas[i].pregunta.idSimuladorTipoRespuesta==1) {
+      this.ListaPreguntas[i].pregunta.respuesta.forEach((x:any)=>{
+        x.respuestaSelecionada=0
+      })
+      return 1;
+    }
+    if (value == 1 && this.ListaPreguntas[i].pregunta.respuesta[j] && this.ListaPreguntas[i].pregunta.idSimuladorTipoRespuesta==1) {
+      return 0;
+    }
+    if (value == 0 && this.ListaPreguntas[i].pregunta.respuesta[j] && this.ListaPreguntas[i].pregunta.idSimuladorTipoRespuesta==5) {
+      return 1;
+    }
+    if (value == 1 && this.ListaPreguntas[i].pregunta.respuesta[j] && this.ListaPreguntas[i].pregunta.idSimuladorTipoRespuesta==5) {
       return 0;
 
-  }
+    }
+    else return 0;
+}
+VerificarMarcado(i:number){
+  this.RespuestaMarcada=false
+  this.ListaPreguntas[i].pregunta.respuesta.forEach((x:any)=>{
+    if( x.respuestaSelecionada==1){
+      this.RespuestaMarcada=true
+    }
+  })
+}
   RegresarMenu(i:number){
     this.Retroalimentacion=false;
     this.PausarContador=true;
@@ -141,23 +151,24 @@ export class EstudioPreguntaComponent implements OnInit {
     this.RegistroEnvioRespuesta.idSimuladorTipoRespuesta=this.ListaPreguntas[i].pregunta.idSimuladorTipoRespuesta,
     this.ListaPreguntas[i].pregunta.respuesta.forEach((x:any)=>{
       if(x.respuestaSelecionada==1){
-        this.DetalleRespuestaEnvio.idSimuladorAdsaPreguntaRespuesta=x.id;
-        this.DetalleRespuestaEnvio.id=this.ListaPreguntas[i].id;
-        this.DetalleRespuestaEnvio.idSimuladorAdsaExamen=0;
-        this.DetalleRespuestaEnvio.idSimuladorAdsaDominio=0;
-        this.DetalleRespuestaEnvio.idSimuladorAdsaTarea=0;
-        this.DetalleRespuestaEnvio.idSimuladorAdsaPregunta=this.ListaPreguntas[i].idSimuladorAdsaPregunta;
-        this.DetalleRespuestaEnvio.ejecutado=false;
-        this.DetalleRespuestaEnvio.puntaje=0;
-        this.DetalleRespuestaEnvio.idAspNetUsers='';
-        this.DetalleRespuestaEnvio.usuario=''
         if(this.ContadorPreguntaActual<=this.ContadorAux){
           this.RegistroEnvioRespuesta.estadoExamen=2
         }
         else{
           this.RegistroEnvioRespuesta.estadoExamen=3
         }
-        this.RegistroEnvioRespuesta.respuestaDetalle.push(this.DetalleRespuestaEnvio)
+        this.RegistroEnvioRespuesta.respuestaDetalle.push({
+          idSimuladorAdsaPreguntaRespuesta:x.id,
+          id:this.ListaPreguntas[i].id,
+          idSimuladorAdsaExamen:0,
+          idSimuladorAdsaDominio:0,
+          idSimuladorAdsaTarea:0,
+          idSimuladorAdsaPregunta:this.ListaPreguntas[i].idSimuladorAdsaPregunta,
+          ejecutado:false,
+          puntaje:0,
+          idAspNetUsers:'',
+          usuario:''
+        })
       }
 
     })
--- src/app/auth/ModoExamen/ExamenPregunta/examen-pregunta.component.ts
+++ src/app/auth/ModoExamen/ExamenPregunta/examen-pregunta.component.ts
@@ -70,8 +70,6 @@ export class ExamenPreguntaComponent implements OnInit {
     idAspNetUsers:'',
     usuario:''
   }
-  /* public DetalleRespuestaEnvio:any */
-  public Retroalimentacion= false;
   public RespuestaCorrecta=false;
   public RespuestaMarcada=false;
   public PausarContador=true;
@@ -108,24 +106,34 @@ export class ExamenPreguntaComponent implements OnInit {
     })
   }
   chageRadio(value: number,i: number, j: number) {
-    this.RespuestaMarcada=false;
-      if (value == 0 && this.ListaPreguntas[i].pregunta.respuesta[j] && this.ListaPreguntas[i].pregunta.idSimuladorTipoRespuesta==1) {
-        this.ListaPreguntas[i].pregunta.respuesta.forEach((x:any)=>{
-          x.respuestaSelecionada=0
-        })
-        this.RespuestaMarcada=true;
-        return 1;
-      }
-      if (value == 0 && this.ListaPreguntas[i].pregunta.respuesta[j] && this.ListaPreguntas[i].pregunta.idSimuladorTipoRespuesta==5) {
-        this.RespuestaMarcada=true
-        return 1;
-      }
-      this.RespuestaMarcada=false;
+    if (value == 0 && this.ListaPreguntas[i].pregunta.respuesta[j] && this.ListaPreguntas[i].pregunta.idSimuladorTipoRespuesta==1) {
+      this.ListaPreguntas[i].pregunta.respuesta.forEach((x:any)=>{
+        x.respuestaSelecionada=0
+      })
+      return 1;
+    }
+    if (value == 1 && this.ListaPreguntas[i].pregunta.respuesta[j] && this.ListaPreguntas[i].pregunta.idSimuladorTipoRespuesta==1) {
       return 0;
+    }
+    if (value == 0 && this.ListaPreguntas[i].pregunta.respuesta[j] && this.ListaPreguntas[i].pregunta.idSimuladorTipoRespuesta==5) {
+      return 1;
+    }
+    if (value == 1 && this.ListaPreguntas[i].pregunta.respuesta[j] && this.ListaPreguntas[i].pregunta.idSimuladorTipoRespuesta==5) {
+      return 0;
+
+    }
+    else return 0;
+}
+VerificarMarcado(i:number){
+  this.RespuestaMarcada=false
+  this.ListaPreguntas[i].pregunta.respuesta.forEach((x:any)=>{
+    if( x.respuestaSelecionada==1){
+      this.RespuestaMarcada=true
+    }
+  })
 }
 RegresarMenu(i:number){
   this.EnviarRespuesta(i);
-  this.Retroalimentacion=false;
   this.PausarContador=true;
   this._router.navigate(['/ModoExamen']);
 }
@@ -143,23 +151,24 @@ EnviarRespuesta(i:number){
   this.RegistroEnvioRespuesta.idSimuladorTipoRespuesta=this.ListaPreguntas[i].pregunta.idSimuladorTipoRespuesta,
   this.ListaPreguntas[i].pregunta.respuesta.forEach((x:any)=>{
     if(x.respuestaSelecionada==1){
-      this.DetalleRespuestaEnvio.idSimuladorAdsaPreguntaRespuesta=x.id;
-      this.DetalleRespuestaEnvio.id=this.ListaPreguntas[i].id;
-      this.DetalleRespuestaEnvio.idSimuladorAdsaExamen=0;
-      this.DetalleRespuestaEnvio.idSimuladorAdsaDominio=0;
-      this.DetalleRespuestaEnvio.idSimuladorAdsaTarea=0;
-      this.DetalleRespuestaEnvio.idSimuladorAdsaPregunta=this.ListaPreguntas[i].idSimuladorAdsaPregunta;
-      this.DetalleRespuestaEnvio.ejecutado=false;
-      this.DetalleRespuestaEnvio.puntaje=0;
-      this.DetalleRespuestaEnvio.idAspNetUsers='';
-      this.DetalleRespuestaEnvio.usuario=''
       if(this.ContadorPreguntaActual<=this.ContadorAux){
         this.RegistroEnvioRespuesta.estadoExamen=2
       }
       else{
         this.RegistroEnvioRespuesta.estadoExamen=3
       }
-      this.RegistroEnvioRespuesta.respuestaDetalle.push(this.DetalleRespuestaEnvio)
+      this.RegistroEnvioRespuesta.respuestaDetalle.push({
+        idSimuladorAdsaPreguntaRespuesta:x.id,
+        id:this.ListaPreguntas[i].id,
+        idSimuladorAdsaExamen:0,
+        idSimuladorAdsaDominio:0,
+        idSimuladorAdsaTarea:0,
+        idSimuladorAdsaPregunta:this.ListaPreguntas[i].idSimuladorAdsaPregunta,
+        ejecutado:false,
+        puntaje:0,
+        idAspNetUsers:'',
+        usuario:''
+      })
     }
 
   })
@@ -167,21 +176,10 @@ EnviarRespuesta(i:number){
     next:(x)=>{
       this.RespuestaCorrecta=x
     },
-    complete:()=>{
-      this.Retroalimentacion=true
-    },
   })
   this.RespuestaMarcada=false
-}
- SalirRetroalimentacion(){
-  this.PausarContador=true;
-    this._router.navigate(['/ModoExamen']);
-    this.ContadorPregunta=this.ContadorPregunta+1;
-  }
-  SiguientePregunta(){
-    this.ContadorPregunta=this.ContadorPregunta+1;
+  this.ContadorPregunta=this.ContadorPregunta+1;
     this.ContadorPreguntaActual=this.ContadorPreguntaActual+1;
-    this.Retroalimentacion=false;
     if (this.ContadorPreguntaActual>this.CantidadTotalPreguntas){
       this.PausarContador=true;
       this._router.navigate(['/ModoExamen/ExamenReporte/'+this.IdExamen]);
