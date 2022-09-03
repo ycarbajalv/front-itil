2d3ff33a6cf354747716c376256c4bbcb73b56ff
Max Mantilla Rodríguez <mmantilla@bsginstitute.com>
2022-09-03 12:02:20 -0500

    publicacion

--- src/app/shared/Charts/Barras/barras.component.ts
+++ src/app/shared/Charts/Barras/barras.component.ts
@@ -62,7 +62,7 @@ export class BarrasComponent implements OnInit {
       scales: {
         x: {},
         y: {
-          min: 10
+          min: 0
         }
       },
       plugins: {
@@ -102,7 +102,7 @@ export class BarrasComponent implements OnInit {
       scales: {
         x: {},
         y: {
-          min: 10
+          min: 0
         }
       },
       plugins: {
